1c49be1876e1878be7f96de829c8c44af23db1a9
Joe Casabona <jcasabona@gmail.com>
2017-03-20 11:53:03 -0400

    Answer to Hello World Exercise

---
+++ hello.php
@@ -0,0 +1,20 @@
+<?php
+
+// Hello World Answer from PHP: A Primer by Joe Casabona
+
+// Step 1: "Hello World" as a String Literal
+echo "Hello World";
+
+echo "<br/>";
+
+// Step 2: "Hello World" as a variable
+$hello = "Hello World";
+echo $hello;
+
+echo "<br/>";
+
+// Step 3: "Hello" and "World" as array entities. 
+$hello = array( "Hello", "World" );
+echo $hello[0] . " " . $hello[1];
+
+?>
